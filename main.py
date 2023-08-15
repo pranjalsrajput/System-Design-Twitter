674831702228766a47128557b704b908215dd5ec
pranjalsrajput001 <pranjal@kaios.ai>
2023-08-15 23:53:02 +0200

    added the explaination for the get_news_feed method logic

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ follow
 unfollow
 get_news_feed -> 10 recent tweets of followees
 """
-from collections import defaultdict
 import heapq
+from collections import defaultdict
 
 
 class Twitter:
@@ -34,6 +34,16 @@ class Twitter:
             self.follow_map[follower_id].remove(followee_id)
 
     def get_news_feed(self, user_id: int) -> []:
+        """
+        We use minheap to find the latest 10 tweets of the followees of the given follower/user.
+        We first retrieve the latest tweets of all the followees of the follower. Then we compare the timestamp (
+        which is represented by count here) of these tweets and select the one with the minimum value. We then
+        decrease the index of this followees tweet list to get its previous tweet, and then compare this tweet's
+        timestamp with the timstamp of the other two tweets and then selects the one with the minimum value. This
+        process is repeated until we have maximum of 10 tweets.
+        :param user_id:
+        :return:
+        """
         min_heap = []
         result = []
         self.follow_map[user_id].add(user_id)
@@ -56,7 +66,6 @@ class Twitter:
 
 
 twitter = Twitter()
-
 
 twitter.follow(1, 2)
 twitter.follow(1, 3)
